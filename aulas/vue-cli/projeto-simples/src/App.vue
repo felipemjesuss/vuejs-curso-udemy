9b88fb5bc1d825241d61c73198993ae7cf489ad6
Felipe Martins <felipemjesus@outlook.com>
2020-03-28 12:24:00 -0300

    Iniciando com vue cli

---
+++ aulas/vue-cli/projeto-simples/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        {{ titulo }}
+    </div>
+</template>
+
+<script>
+export default {
+    data: () => {
+        return {
+            titulo: 'Teste Data usando Vue JS!!!'
+        }
+    }
+}
+</script>

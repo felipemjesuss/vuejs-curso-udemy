f50ae925e7991fb50579f2c54b7f0e86639c8f0f
Felipe Martins <felipemjesus@outlook.com>
2020-03-28 18:09:50 -0300

    Desafio componente

---
+++ desafio-06-componente/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+	<v-app>
+		<Navigarion />
+
+		<Toolbar />
+
+		<Content />
+
+		<Footer />
+	</v-app>
+</template>
+
+<script>
+import Navigarion from './Navigation.vue'
+import Toolbar from './Toolbar.vue'
+import Content from './Content.vue'
+import Footer from './Footer.vue'
+
+export default {
+	components: { 
+		Navigarion,
+		Toolbar,
+		Content,
+		Footer
+	}
+}
+</script>
